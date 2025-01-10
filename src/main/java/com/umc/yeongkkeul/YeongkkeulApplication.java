55e8356e8c5e00f6cc4053f4aba1d47fb5db3d9c
Jsh <king601143@gmail.com>
2025-01-10 13:16:28 +0900

    feat: 프로젝트 초기설정 #1

---
+++ src/main/java/com/umc/yeongkkeul/YeongkkeulApplication.java
@@ -0,0 +1,13 @@
+package com.umc.yeongkkeul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeongkkeulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeongkkeulApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/umc/yeongkkeul/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,8 @@
+package com.umc.yeongkkeul.apiPayload.code;
+
+public interface BaseErrorCode {
+
+    ErrorReasonDTO getReason();
+
+    ErrorReasonDTO getReasonHttpStatus();
+}
