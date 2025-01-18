338dd6059847046243f74c9da5d2ea24a50c93f0
nicehcy <nicehcy2@naver.com>
2025-01-18 13:54:19 +0900

    [FEAT] 상점 아이템, 아이템 구매 이력 Entity 추가

---
+++ src/main/java/com/umc/yeongkkeul/domain/enums/ItemType.java
@@ -0,0 +1,9 @@
+package com.umc.yeongkkeul.domain.enums;
+
+public enum ItemType {
+
+    SWING,
+    TOY,
+    BOWL,
+    NEST
+}
---
+++ src/main/java/com/umc/yeongkkeul/domain/mapping/Purchase.java
@@ -0,0 +1,41 @@
+package com.umc.yeongkkeul.domain.mapping;
+
+import com.umc.yeongkkeul.domain.Item;
+import com.umc.yeongkkeul.domain.User;
+import com.umc.yeongkkeul.domain.common.BaseEntity;
+import com.umc.yeongkkeul.domain.enums.ItemType;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * 유저가 상점에서 구매한 아이템 이력
+ * User-Item의 중간 테이블
+ */
+@Entity
+public class Purchase extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item itemId;
+
+    @Column(name = "type", nullable = false)
+    private ItemType type;
+
+    @Column(name = "purchased_at", nullable = false)
+    private LocalDateTime purchasedAt;
+
+    @Column(name = "used_reward", nullable = false)
+    private Integer usedReward;
+
+    @Column(name = "is_used", nullable = false)
+    private Boolean isUsed;
+}
