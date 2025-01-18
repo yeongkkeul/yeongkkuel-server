a4b38dba628fca786f122d77a394d2a3d02f90fc
nicehcy <nicehcy2@naver.com>
2025-01-18 13:54:19 +0900

    [FEAT] 상점 아이템, 아이템 구매 이력 Entity 추가

---
+++ src/main/java/com/umc/yeongkkeul/domain/Item.java
@@ -0,0 +1,31 @@
+package com.umc.yeongkkeul.domain;
+
+import com.umc.yeongkkeul.domain.common.BaseEntity;
+import com.umc.yeongkkeul.domain.enums.ItemType;
+import jakarta.persistence.*;
+
+/**
+ * 상점 아이템
+ */
+@Entity
+public class Item extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 20, nullable = false)
+    private String name;
+
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ItemType type;
+
+    @Column(name = "price", nullable = false)
+    private Integer price;
+
+    // null 값이면 프론트에서 기본 이미지로 등록
+    @Column(name = "image_url")
+    private String imageUrl;
+}
+
