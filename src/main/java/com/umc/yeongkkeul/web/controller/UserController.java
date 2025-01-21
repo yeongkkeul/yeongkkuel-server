64c8f18b0a95dad49989822f8356f5cebacee7e2
KimMinHwan <2021111977@dgu.ac.kr>
2025-01-22 01:03:19 +0900

    FIX: "yml gitignore 설정 및 refreshToken 구현중"

--- src/main/java/com/umc/yeongkkeul/web/controller/UserController.java
+++ src/main/java/com/umc/yeongkkeul/web/controller/UserController.java
@@ -4,6 +4,7 @@ import com.umc.yeongkkeul.apiPayload.ApiResponse;
 import com.umc.yeongkkeul.security.TokenProvider;
 import com.umc.yeongkkeul.service.KakaoLoginService;
 import com.umc.yeongkkeul.web.dto.KakaoInfoResponseDto;
+import com.umc.yeongkkeul.web.dto.UserRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +26,7 @@ public class UserController {
     @Autowired
     private TokenProvider tokenProvider;
 
+    //카카오
     @GetMapping("/auth/kakao-login/")
     @Operation(summary = "카카오 로그인", description = "카카오 로그인 GET")
     public ApiResponse<KakaoInfoResponseDto.KakaoInfoDTO> kakakoLogin(@RequestParam String code){
@@ -38,13 +40,25 @@ public class UserController {
             return ApiResponse.onFailure("4000", "Email not found in Kakao account", null);
         }
 
-        log.info("email2={}",email);
-
-        String jwtToken = tokenProvider.genrateToken(email);
+        String jwtToken = tokenProvider.genrateToken(email).getAccessToken();
+        String refreshToken = tokenProvider.genrateToken(email).getRefreshToken();
 
         KakaoInfoResponseDto.KakaoInfoDTO kakaoInfoDTO = kakaoLoginService.loginUserInfo(jwtToken,email,name);
 
         return ApiResponse.onSuccess(kakaoInfoDTO);
     }
+
+
+    //구글
+
+
+
+
+    //사용자 정보기입
+//    @PutMapping("/auth/user-info")
+//    @Operation(summary = "사용자 정보 기입", description = "사용자 정보기입 소셜 로그인 후 기본 정보 저장하고 PUT으로 값 추가하는 형태")
+//    public ApiResponse<?> saveUserInfo(@RequestBody UserRequestDto.userInfoDto userInfoDto){
+//
+//    }
 
 }
