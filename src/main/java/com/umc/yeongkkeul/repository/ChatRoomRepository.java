2f3ebe292cb601d8b9d7cab4478e3d9c7a81b370
jjin70 <jjin0434@naver.com>
2025-02-09 00:56:35 +0900

    feat: 채팅방 배너 조회 구현 #62

--- src/main/java/com/umc/yeongkkeul/repository/ChatRoomRepository.java
+++ src/main/java/com/umc/yeongkkeul/repository/ChatRoomRepository.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
     @Query("SELECT c FROM ChatRoom c " +
@@ -23,4 +25,13 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
                                          Pageable pageable);
 
     Page<ChatRoom> findByTitleContainingOrderByParticipationCountDesc(String title, Pageable pageable);
+
+    @Query("SELECT c FROM ChatRoom c WHERE c.id IN :chatRoomIds")
+    List<ChatRoom> findAllByIdIn(@Param("chatRoomIds") List<Long> chatRoomIds);
+
+    List<ChatRoom> findAllByAgeGroupFilterAndJobFilterOrderByTotalScoreDesc(AgeGroup ageGroupFilter, Job jobFilter);
+
+    List<ChatRoom> findAllByAgeGroupFilterOrderByTotalScoreDesc(AgeGroup ageGroupFilter);
+
+    List<ChatRoom> findAllByJobFilterOrderByTotalScoreDesc(Job jobFilter);
 }
