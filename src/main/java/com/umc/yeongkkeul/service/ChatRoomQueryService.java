9c7a8d31268fb27da2e0d04f77da3337e3f1c40f
jjin70 <jjin0434@naver.com>
2025-02-13 17:25:06 +0900

    feat: 채팅방 배너 조회 api 수정 #120

--- src/main/java/com/umc/yeongkkeul/service/ChatRoomQueryService.java
+++ src/main/java/com/umc/yeongkkeul/service/ChatRoomQueryService.java
@@ -1,111 +1,30 @@
 package com.umc.yeongkkeul.service;
 
 import com.umc.yeongkkeul.apiPayload.code.status.ErrorStatus;
-import com.umc.yeongkkeul.apiPayload.exception.handler.ChatRoomHandler;
+import com.umc.yeongkkeul.apiPayload.exception.GeneralException;
 import com.umc.yeongkkeul.domain.ChatRoom;
-import com.umc.yeongkkeul.domain.Expense;
-import com.umc.yeongkkeul.domain.enums.AgeGroup;
-import com.umc.yeongkkeul.domain.enums.Job;
-import com.umc.yeongkkeul.repository.ChatRoomMembershipRepository;
 import com.umc.yeongkkeul.repository.ChatRoomRepository;
-import com.umc.yeongkkeul.repository.ExpenseRepository;
 import com.umc.yeongkkeul.web.dto.BannerResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Collections;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ChatRoomQueryService {
 
     private final ChatRoomRepository chatRoomRepository;
-    private final ChatRoomMembershipRepository chatRoomMembershipRepository;
-    private final ExpenseRepository expenseRepository;
 
-    // TODO : 배너 api 배치를 통해 저장된 값 보여주도록 수 (배너에서는 필터 없으면 null로 / 채팅방 조회에서는 필터 없으면 전체 백분위로)
     public BannerResponseDto getChatRoomBanner(Long chatRoomId) {
 
         ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
-                .orElseThrow(() -> new ChatRoomHandler(ErrorStatus._CHATROOM_NOT_FOUND));
+            .orElseThrow(() -> new GeneralException(ErrorStatus._CHATROOM_NOT_FOUND));
 
-        // 해당 채팅방의 유저 리스트
-        List<Long> userIdList = chatRoomMembershipRepository.findUserIdByChatroomId(chatRoomId);
-
-        int achievingCount = 0; // 하루 목표 달성한 유저 수
-        int sumAmount = 0; // 지출 통계를 위한 합계
-        for (Long userId : userIdList) {
-            LocalDate yesterday = LocalDate.now().minusDays(1);
-            List<Expense> expenseList = expenseRepository.findYesterdayExpenseByUserId(userId, yesterday);
-
-            int userAmount = 0; // 유저의 하루 지출 총액
-            for (Expense expense : expenseList) {
-                userAmount += expense.getAmount();
-            }
-
-            // 하루 목표 달성한 유저 수 count (유저의 하루 지출 총액 <= 채팅방의 하루 목표 지출 금액)
-            if (userAmount <= chatRoom.getDailySpendingGoalFilter()) {
-                achievingCount++;
-            }
-            sumAmount += userAmount;
-        }
-
-        // 채팅방 총 참여자 수
-        int chatRoomUserCount = chatRoomMembershipRepository.countByChatroomId(chatRoomId);
-
-        // 지출 평균
-        int avgAmount = sumAmount / chatRoomUserCount;
-
-        // age 필터
-        AgeGroup ageFilter = chatRoom.getAgeGroupFilter();
-        String age = (ageFilter != null) ? ageFilter.getAgeGroup() : null;
-
-        // job 필터
-        Job jobFilter = chatRoom.getJobFilter();
-        String job = (jobFilter != null) ? jobFilter.getJob() : null;
-
-        // job, age 기준으로 필터링
-        List<ChatRoom> chatRoomList = Collections.emptyList();;
-        if (ageFilter != null && !"UNDECIDED".equals(ageFilter) && jobFilter != null && !"UNDECIDED".equals(jobFilter)) { // age, job
-            chatRoomList = chatRoomRepository.findAllByAgeGroupFilterAndJobFilterOrderByTotalScoreDesc(ageFilter, jobFilter);
-        } else if (ageFilter != null && !"UNDECIDED".equals(ageFilter) && (jobFilter == null || "UNDECIDED".equals(jobFilter))) { // age
-            chatRoomList = chatRoomRepository.findAllByAgeGroupFilterOrderByTotalScoreDesc(ageFilter);
-        } else if (jobFilter != null && !"UNDECIDED".equals(jobFilter) && (ageFilter == null || "UNDECIDED".equals(ageFilter))) { // job
-            chatRoomList = chatRoomRepository.findAllByJobFilterOrderByTotalScoreDesc(jobFilter);
-        }
-
-        // null 아닌 방만 남기기
-        List<ChatRoom> chatRooms = chatRoomList.stream()
-                .filter(room -> room.getTotalScore() != null)
-                .toList();
-
-        // 채팅방 백분위 계산 (chatRoom totalScore Batch 돌린 값)
-        Double topRate = null; // age, job 없을 때, 5명 이하일 때 null 반환 위함
-        if (chatRooms.size() >= 5) { // 5명 이상인 방만 계산
-            int ranking = 1;
-            for (int i = 0; i < chatRooms.size(); i++) {
-                if (chatRooms.get(i).getId().equals(chatRoom.getId())) {
-                    ranking = i + 1;
-                    break;
-                }
-            }
-            topRate = ((double) ranking / chatRooms.size()) * 100.0;
-        }
-
-        // 배너 기준 날짜 (공지하는 날짜 - 오늘)
+        // 배너 기준 날짜 (공지하는 날짜 = 오늘)
         String createdAt = LocalDate.now().format(DateTimeFormatter.ofPattern("MM.dd"));
 
-        return BannerResponseDto.builder()
-                .achievingCount(achievingCount)
-                .chatRoomUserCount(chatRoomUserCount)
-                .avgAmount(avgAmount)
-                .age(age)
-                .job(job)
-                .topRate(topRate)
-                .createdAt(createdAt)
-                .build();
+        return BannerResponseDto.from(chatRoom, createdAt);
     }
 }
--- src/main/java/com/umc/yeongkkeul/web/dto/BannerResponseDto.java
+++ src/main/java/com/umc/yeongkkeul/web/dto/BannerResponseDto.java
@@ -1,5 +1,6 @@
 package com.umc.yeongkkeul.web.dto;
 
+import com.umc.yeongkkeul.domain.ChatRoom;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,11 +8,23 @@ import lombok.Getter;
 @Builder
 public class BannerResponseDto {
 
-    private int achievingCount;
-    private int chatRoomUserCount;
-    private int avgAmount;
-    private String age;
-    private String job;
-    private Double topRate;
-    private String createdAt;
+    private Integer achievingCount; // 목표 달성 챌린저 수
+    private Integer chatRoomUserCount; // 채팅방 내 챌린저 수
+    private Integer avgAmount; // 채팅방의 지출 평균
+    private String age; // 나이대
+    private String job; // 직업대
+    private Double topRate; // 상위 백분위
+    private String createdAt; // 공지 날짜
+
+    public static BannerResponseDto from(ChatRoom chatRoom, String createdAt) {
+        return BannerResponseDto.builder()
+                .achievingCount(chatRoom.getAchievedCount())
+                .chatRoomUserCount(chatRoom.getParticipationCount())
+                .avgAmount(chatRoom.getAverageExpense())
+                .age(chatRoom.getAgeGroupFilter().getAgeGroup())
+                .job(chatRoom.getJobFilter().getJob())
+                .topRate((chatRoom.getAgeGroupFilter().equals("UNDECIDED")&&chatRoom.getJobFilter().equals("UNDECIDED")) ? null : chatRoom.getRanking())
+                .createdAt(createdAt)
+                .build();
+    }
 }
