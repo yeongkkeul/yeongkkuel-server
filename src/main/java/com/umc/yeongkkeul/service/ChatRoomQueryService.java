d17f2ac1d900a97d36593974df7bcd915abe72eb
jjin70 <jjin0434@naver.com>
2025-02-13 17:49:55 +0900

    fix: 예외 처리 handler 수정 #120

--- src/main/java/com/umc/yeongkkeul/service/ChatRoomQueryService.java
+++ src/main/java/com/umc/yeongkkeul/service/ChatRoomQueryService.java
@@ -2,6 +2,7 @@ package com.umc.yeongkkeul.service;
 
 import com.umc.yeongkkeul.apiPayload.code.status.ErrorStatus;
 import com.umc.yeongkkeul.apiPayload.exception.GeneralException;
+import com.umc.yeongkkeul.apiPayload.exception.handler.ChatRoomHandler;
 import com.umc.yeongkkeul.domain.ChatRoom;
 import com.umc.yeongkkeul.repository.ChatRoomRepository;
 import com.umc.yeongkkeul.web.dto.BannerResponseDto;
@@ -20,7 +21,7 @@ public class ChatRoomQueryService {
     public BannerResponseDto getChatRoomBanner(Long chatRoomId) {
 
         ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
-            .orElseThrow(() -> new GeneralException(ErrorStatus._CHATROOM_NOT_FOUND));
+            .orElseThrow(() -> new ChatRoomHandler(ErrorStatus._CHATROOM_NOT_FOUND));
 
         // 배너 기준 날짜 (공지하는 날짜 = 오늘)
         String createdAt = LocalDate.now().format(DateTimeFormatter.ofPattern("MM.dd"));
