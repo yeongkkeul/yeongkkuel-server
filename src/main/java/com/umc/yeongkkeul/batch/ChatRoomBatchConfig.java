e9d28e1cbaf6bee1891ad395d05794b56ba4b776
jjin70 <jjin0434@naver.com>
2025-02-14 13:20:26 +0900

    fix: ItemProcessor 분리 #120

--- src/main/java/com/umc/yeongkkeul/batch/ChatRoomBatchConfig.java
+++ src/main/java/com/umc/yeongkkeul/batch/ChatRoomBatchConfig.java
@@ -18,11 +18,13 @@ import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.batch.item.support.CompositeItemProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
@@ -50,7 +52,7 @@ public class ChatRoomBatchConfig {
         return new StepBuilder("updateChatRoomStep", jobRepository)
                 .<ChatRoom, ChatRoom>chunk(10, transactionManager)
                 .reader(chatRoomItemReader())
-                .processor(chatRoomItemProcessor())
+                .processor(chatRoomCompositeProcessor())
                 .writer(chatRoomItemWriter())
                 .build();
     }
@@ -66,14 +68,8 @@ public class ChatRoomBatchConfig {
     }
 
     @Bean
-    public ItemProcessor<ChatRoom, ChatRoom> chatRoomItemProcessor() {
+    public ItemProcessor<ChatRoom, ChatRoom> expenseProcessor() { // 목표 달성 인원, 지출 평균 계산
         return chatRoom -> {
-
-            // ------------------------------
-            // 채팅방 내 목표 달성 인원, 지출 평균 계산
-            // ------------------------------
-
-            // 특정 채팅방의 유저 리스트
             List<Long> userIdList = chatRoomMembershipRepository.findUserIdByChatroomId(chatRoom.getId());
 
             int achievedCount = 0; // 하루 목표 달성한 유저 수
@@ -89,7 +85,6 @@ public class ChatRoomBatchConfig {
 
                 int totalExpenditure = totalExpenditureLong.intValue();
 
-                // 하루 목표 달성한 유저 수 count (유저의 하루 지출 총액 <= 채팅방의 하루 목표 지출 금액)
                 if (totalExpenditure <= chatRoom.getDailySpendingGoalFilter()) {
                     achievedCount++;
                 }
@@ -106,9 +101,16 @@ public class ChatRoomBatchConfig {
             chatRoom.setAchievedCount(achievedCount); // 목표 달성 인원 설정
             chatRoom.setAverageExpense(averageExpense); // 지출 평균 설정
 
-            // ------------------------------
-            // 채팅방 랭킹 (백분위) 계산
-            // ------------------------------
+            return chatRoom;
+        };
+    }
+
+    @Bean
+    public ItemProcessor<ChatRoom, ChatRoom> rankingProcessor() { // 백분위 계산 로직
+        return chatRoom -> {
+            if (chatRoom.getTotalScore() == null) { // totalScore가 null인 경우(5명 이하인 채팅방은 null)
+                return null;
+            }
 
             AgeGroup ageFilter = chatRoom.getAgeGroupFilter(); // age 필터
             com.umc.yeongkkeul.domain.enums.Job jobFilter = chatRoom.getJobFilter(); // job 필터
@@ -127,20 +129,31 @@ public class ChatRoomBatchConfig {
                 chatRoomList = chatRoomRepository.findAllByOrderByTotalScoreDesc();
             }
 
-            // 채팅방 백분위 계산
-            int ranking = chatRoomList.stream()
-                    .filter(c -> c.getId().equals(chatRoom.getId()))
-                    .map(chatRoomList::indexOf)
-                    .findFirst()
-                    .map(i -> i + 1) // 인덱스 1부터 시작
-                    .orElse(0);
+            int rank = 1;
+            for (int i = 0; i < chatRoomList.size(); i++) {
+                if (chatRoomList.get(i).getId().equals(chatRoom.getId())) {
+                    rank = i + 1;
+                    break;
+                }
+            }
 
-            int topRate = (int) Math.round(((double) ranking / chatRoomList.size()) * 100); // 소수점 반올림
-
+            int topRate = (int) Math.round(((double) rank / chatRoomList.size()) * 100);
             chatRoom.setRanking(topRate);
 
             return chatRoom;
         };
+    }
+
+    @Bean
+    public CompositeItemProcessor<ChatRoom, ChatRoom> chatRoomCompositeProcessor() {
+        List<ItemProcessor<ChatRoom, ChatRoom>> processors = new ArrayList<>();
+        processors.add(expenseProcessor());  // 목표 달성 인원 및 지출 평균 계산
+        processors.add(rankingProcessor());  // 백분위 계산
+
+        CompositeItemProcessor<ChatRoom, ChatRoom> processor = new CompositeItemProcessor<>();
+        processor.setDelegates(processors);
+
+        return processor;
     }
 
     @Bean
